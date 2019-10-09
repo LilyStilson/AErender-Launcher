29cd5f80df9953315b8f9b951449a957f4458b90
Alice <42270214+LilyStilson@users.noreply.github.com>
2019-10-09 21:42:40 +1000

    Add files via upload

--- src/AErender.dpr
+++ src/AErender.dpr
@@ -13,7 +13,8 @@ uses
   Unit4 in 'Unit4.pas' {Form4},
   Unit5 in 'Unit5.pas' {Form5},
   Unit6 in 'Unit6.pas' {Form6},
-  RenderingUnit in 'RenderingUnit.pas' {RenderingForm};
+  RenderingUnit in 'RenderingUnit.pas' {RenderingForm},
+  MathExpParser in 'MathExpParser.pas';
 
 {$R *.res}
 
