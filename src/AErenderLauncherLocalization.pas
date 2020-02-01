8be37d384abd758558bd1d14f3cb1e84aab287a5
LilyStilson <lunamusicoff@gmail.com>
2020-02-01 13:03:58 +1000

    Commit via Git.

---
+++ src/AErenderLauncherLocalization.pas
@@ -0,0 +1,22 @@
+unit AErenderLauncherLocalization;
+
+interface
+
+type
+  MainFormLanguage = record
+
+  end;
+
+  SettingsFormLanguage = record
+
+  end;
+
+  LauncherLanguage = record
+    MainForm: MainFormLanguage;
+    SettingsForm: SettingsFormLanguage;
+    
+  end;
+
+implementation
+
+end.
