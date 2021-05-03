6e074ce76a16754f37b29ddb1493099aa803a203
kostyaromanets2@gmail.com <kostyaromanets2@gmail.com>
2021-05-04 02:32:05 +1000

    Updated aeparser plugin

--- src/lib/plugins/AEP Parser/aeparser.go
+++ src/lib/plugins/AEP Parser/aeparser.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"os"
 
 	aep "github.com/LilyStilson/aftereffects-aep-parser"
 )
@@ -13,9 +14,11 @@ func main() {
 		
 	var comps []aep.Item
 
-	for _, value := range project.Items 
-		if value.ItemType == aep.ItemTypeComposition 
+	for _, value := range project.Items {
+		if value.ItemType == aep.ItemTypeComposition {
 			comps = append(comps, *value)
+		}
+	}
 	
 	JSONComp, err := json.Marshal(comps)
 
---
+++ src/lib/plugins/AEP Parser/go.mod
@@ -0,0 +1,8 @@
+module main
+
+go 1.10
+
+require (
+	github.com/LilyStilson/aftereffects-aep-parser v0.0.0-20210220175541-6a3310917538
+	github.com/rioam2/rifx v0.0.0-20200718170510-a8114e272da2 // indirect
+)
