403b31a060aee03791b9c791d080229ce6016d99
kostyaromanets2@gmail.com <kostyaromanets2@gmail.com>
2021-06-05 21:45:19 +1000

    Added a build command for easier compilation

---
+++ build/build.bat
@@ -0,0 +1,9 @@
+@ECHO off
+@setlocal
+
+set "msbuild_path=%WINDIR%\Microsoft.NET\Framework\v4.0.30319"
+set "BDS=C:\Program Files (x86)\Embarcadero\Studio\21.0\"
+
+::"%msbuild_path%\msbuild.exe" /t:build src\AErender_Launcher.dproj
+
+"%msbuild_path%\msbuild.exe" /t:build AErender_Launcher.ciproj
